ee1c3c3a64bd2ac97ce31284bc6502a9508b1648
LotP1 <rasmus.stilling.pedersen1@gmail.com>
2024-12-14 15:01:33 +0100

    default value of dishCacheSelector should be null

--- src/Ryujinx.HLE/Loaders/Processes/ProcessLoader.cs
+++ src/Ryujinx.HLE/Loaders/Processes/ProcessLoader.cs
@@ -235,7 +235,7 @@ namespace Ryujinx.HLE.Loaders.Processes
                                                                        dummyExeFs.GetNpdm(),
                                                                        nacpData,
                                                                        diskCacheEnabled: false,
-                                                                       diskCacheSelector: string.Empty,
+                                                                       diskCacheSelector: null,
                                                                        allowCodeMemoryForJit: true,
                                                                        programName,
                                                                        programId,
