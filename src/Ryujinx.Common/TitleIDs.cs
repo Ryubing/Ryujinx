35898165ac1f123cbb64bb82254e6aa1136b9370
Otozinclus <58051309+Otozinclus@users.noreply.github.com>
2025-01-21 05:19:50 +0100

    Update TitleIDs.cs

--- src/Ryujinx.Common/TitleIDs.cs
+++ src/Ryujinx.Common/TitleIDs.cs
@@ -28,22 +28,29 @@ namespace Ryujinx.Common
         
         public static readonly string[] GreatMetalTitles =
         [
-            "01006f8002326000", // Animal Crossings: New Horizons
-            "010076f0049a2000", // Bayonetta
-            "01009bf0072d4000", // Captain Toad: Treasure Tracker
             "0100a5c00d162000", // Cuphead
             "010023800d64a000", // Deltarune
-            "01003a30012c0000", // LEGO City Undercover
             "010028600EBDA000", // Mario 3D World
             "0100152000022000", // Mario Kart 8 Deluxe
-            "01005CA01580E000", // Persona 5
             "0100187003A36000", // Pokémon: Let's Go, Eevee!
             "010003f003a34000", // Pokémon: Let's Go, Pikachu!
             "01008C0016544000", // Sea of Stars
             "01006A800016E000", // Smash Ultimate
-            "010015100b514000", // Super Mario Bros. Wonder
-            "0100000000010000", // Super Mario Odyessy
             "01006bb00c6f0000", // The Legend of Zelda: Link's Awakening
+
+            //These ones have small issues, but those happen on Vulkan as well:
+            "01006f8002326000", // Animal Crossings: New Horizons
+            "01009bf0072d4000", // Captain Toad: Treasure Tracker
+            "01005CA01580E000", // Persona 5 Royale
+            "0100000000010000", // Super Mario Odyssey
+
+            //More testing needed:
+            "010076f0049a2000", // Bayonetta
+            "01009510001ca000", // Fast RMX
+            "01003a30012c0000", // LEGO City Undercover
+            "0100704000B3A000", // Snipperclips
+            "01008e20047dc000", // Snipperclips Plus
+            "010015100b514000", // Super Mario Bros. Wonder
         ];
         
         public static string GetDiscordGameAsset(string titleId) 
