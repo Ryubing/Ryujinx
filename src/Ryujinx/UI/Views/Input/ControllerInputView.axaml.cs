e400fdd16831914638891032197c757a17d4bb65
Otozinclus <Otozinclus@gmx.de>
2025-01-25 23:41:57 +0100

    restore prior formatting 1

--- src/Ryujinx/UI/Views/Input/ControllerInputView.axaml.cs
+++ src/Ryujinx/UI/Views/Input/ControllerInputView.axaml.cs
@@ -262,7 +262,6 @@ namespace Ryujinx.Ava.UI.Views.Input
             if (DataContext is not ControllerInputViewModel cVm) return;
             if (!cVm.Config.EnableLedChanging) return;
             if (cVm.Config.TurnOffLed) return;
-            
             cVm.ParentModel.SelectedGamepad.SetLed(cVm.Config.LedColor.ToUInt32());
         }
     }
