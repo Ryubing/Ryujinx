72ccda2a0ad6c524b584872901df5be9ba771f0b
Vudjun <Vudjun@users.noreply.github.com>
2025-02-14 21:10:23 +0000

    Gracefully handle errors in socket creation

--- src/Ryujinx.HLE/HOS/Services/Sockets/Bsd/IClient.cs
+++ src/Ryujinx.HLE/HOS/Services/Sockets/Bsd/IClient.cs
@@ -95,12 +95,21 @@ namespace Ryujinx.HLE.HOS.Services.Sockets.Bsd
                 }
             }
 
-            ISocket newBsdSocket = new ManagedSocket(netDomain, (SocketType)type, protocol, context.Device.Configuration.MultiplayerLanInterfaceId)
-            {
-                Blocking = !creationFlags.HasFlag(BsdSocketCreationFlags.NonBlocking),
-            };
-
             LinuxError errno = LinuxError.SUCCESS;
+            ISocket newBsdSocket;
+
+            try
+            {
+                newBsdSocket = new ManagedSocket(netDomain, (SocketType)type, protocol, context.Device.Configuration.MultiplayerLanInterfaceId)
+                {
+                    Blocking = !creationFlags.HasFlag(BsdSocketCreationFlags.NonBlocking),
+                };
+            }
+            catch (SocketException exception)
+            {
+                var errNo = WinSockHelper.ConvertError((WsaError)exception.ErrorCode);
+                return WriteBsdResult(context, 0, errno);
+            }
 
             int newSockFd = _context.RegisterFileDescriptor(newBsdSocket);
 
